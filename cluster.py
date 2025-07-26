d8e7e77fd49989f11b1b1f9f8eff0d092f43c3e5
Martin <wolpumba@gmail.com>
2025-07-26 23:33:21 +0200

    collect all summaries belonging to one cluster. my goal is to have the ai assign reasonable names for each cluster

--- cluster.py
+++ cluster.py
@@ -29,3 +29,36 @@ plt.scatter(embedding[:, 0], embedding[:, 1], c=scan.labels_, cmap='Spectral', s
 plt.gca().set_aspect('equal', 'datalim')
 plt.savefig('cluster.png')
 
+# >>> scan.labels_
+# array([ 0,  1,  1, ..., -1,  2, 11], shape=(4118,))
+# >>> scan.core_sample_indices_
+# array([   0,    1,    2, ..., 4114, 4116, 4117], shape=(3528,))
+# >>> scan.components_
+# array([[ 6.9252043, -2.7925692],
+#        [ 6.6870832, -3.2808669],
+#        [ 6.6851816, -3.292168 ],
+#        ...,
+#        [ 6.534326 ,  5.427366 ],
+#        [10.893396 , 10.08514  ],
+#        [ 6.2154083,  3.4041855]], shape=(3528, 2), dtype=float32)
+
+df = pd.read_csv('fulltext.csv')
+
+# scan.labels_
+# scan.core_sample_indices_
+# scan.components_
+
+
+# >>> df[scan.labels_==2]
+#                                                 summary
+# 4     **The Real Reasons Behind Stefan Raab's Comeba...
+# 220   **Escaping the "Woke" Mindset: An Interview wi...
+# 375   **Exploring Potential Political Shifts: Nigel ...
+# 394   **The UK's Overqualification Crisis: A Summary...
+# 435   **"Eat the Rich!": A Critical Look at Social I...
+# ...                                                 ...
+# 3909  **Abstract:**\n\nThis video presents a detaile...
+# 3951  **Abstract:**\n\nThis video is a deep-dive inv...
+# 3983  **Abstract:**\n\nThis video explains the key m...
+# 4023  **Abstract:**\n\nThis transcript consists of a...
+# 4116  **Abstract:**\n\nThis video by Gary's Economic...
