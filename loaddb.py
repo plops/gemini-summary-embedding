1a60067763b6da9541293e5506d850b68e8bed3a
Martin <wolpumba@gmail.com>
2025-07-26 15:11:11 +0200

    refactor loaddb.py: reorganize imports and clean up code formatting

--- loaddb.py
+++ loaddb.py
@@ -1,13 +1,12 @@
-from sqlite_minutils import *
-import numpy as np
-import umap
-import umap.plot
-import matplotlib.pyplot as plt
-import seaborn as sns
 import pickle
-import pandas as pd
 
-sns.set(style='white', context='notebook', rc={'figure.figsize':(14,10)})
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import umap.plot
+from sqlite_minutils import *
+
+sns.set(style='white', context='notebook', rc={'figure.figsize': (14, 10)})
 
 debug = False
 
@@ -15,22 +14,21 @@ db = Database("summaries_20250720_embed.db")
 tab = db.table('items')
 # <Table items (identifier, model, transcript, host, summary, summary_done, summary_input_tokens, summary_output_tokens, summary_timestamp_start, summary_timestamp_end, timestamps, timestamps_done, timestamps_input_tokens, timestamps_output_tokens, timestamps_timestamp_start, timestamps_timestamp_end, timestamped_summary_in_youtube_format, cost, original_source_link, include_comments, include_timestamps, include_glossary, output_language, embedding)>
 
-res=[]
-res_text=[]
-res_id=[]
+res = []
+res_text = []
+res_id = []
 for row in tab.rows:
-    emb_bytes=row['embedding']
+    emb_bytes = row['embedding']
     if emb_bytes is not None:
         # load float32 array using numpy from bytes
-        emb=np.frombuffer(emb_bytes, dtype='float32')
+        emb = np.frombuffer(emb_bytes, dtype='float32')
         if debug:
             print(f"{row['identifier']} {emb[0]} {emb[1]} {emb[2]}")
         res.append(emb)
         # I only want the first two lines from the summary
         summarylines = row['summary'].split('\n')
         first_lines = summarylines[:2]
-        res_text.append({"id": row['identifier'],
-                         "summary": " ".join(first_lines)})
+        res_text.append({"id": row['identifier'], "summary": " ".join(first_lines)})
         res_id.append(row['identifier'])
 dft = pd.DataFrame(res_text)
 a = np.array(res)
@@ -50,16 +48,14 @@ if reducer is None:
     reducer.fit(a)
 
     with open(reducer_fn, 'wb') as f:
-        pickle.dump(reducer, f) # 188MB
-
-
+        pickle.dump(reducer, f)  # 188MB
 
 # embedding = reducer.embedding_
 # plt.scatter(embedding[:,0], embedding[:,1], cmap='Spectral', s=5)
 # plt.gca().set_aspect('equal','datalim')
 # plt.colorbar(boundaries=np.arange(11)-.5).set_ticks(np.arange(10))
 
-#umap.plot.points(reducer)
+# umap.plot.points(reducer)
 # umap.plot.connectivity(reducer,show_points=True)
 # # plt.savefig('youtube3.png')
 
