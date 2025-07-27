4ee0d57aa89d8249246467472909ff0150827954
Martin Kielhorn <wolpumba@gmail.com>
2025-07-27 13:20:48 +0200

    Add pandas and matplotlib for enhanced UMAP visualization and clustering analysis
    Will compute UMAP embedding
    [2025-07-27 13:20:16.666] [CUML] [info] Building knn graph using brute force
    UMAP fitting time: 0.429 seconds
    Will compute DBSCAN clustering
    DBSCAN clustering time: 0.008 seconds
    BokehDeprecationWarning: 'circle() method with size value' was deprecated in Bokeh 3.4.0 and will be removed, use 'scatter(size=...) instead' instead.
    Number of clusters found: 139

--- 02_gpu/main.py
+++ 02_gpu/main.py
@@ -2,27 +2,52 @@ import cudf
 from cuml import UMAP
 from cuml.cluster import DBSCAN
 import numpy as np
+import time
+import umap.plot
+import pandas as pd
+from matplotlib import pyplot as plt
+
 # export CUDA_PATH=/opt/cuda/
 
 
 # load a from file
 a = np.load('../01_start/embeddings.npy')
-reducer = UMAP(n_neighbors=4, min_dist=.1, n_components=3)
+reducer3 = UMAP(n_neighbors=4, min_dist=.1, n_components=4)
+reducer2 = UMAP(n_neighbors=4, min_dist=.1, n_components=2)
 print('Will compute UMAP embedding')
 # measure time for fitting
-import time
 start_time = time.time()
-reducer.fit(a)
+reducer3.fit(a)
+reducer2.fit(a)
 end_time = time.time()
 print(f"UMAP fitting time: {end_time - start_time:.3f} seconds")
 
 # Setup and fit clusters
 start_time = time.time()
 print('Will compute DBSCAN clustering')
-scan = DBSCAN(eps=1.0, min_samples=1)
-scan.fit(reducer.embedding_)
+scan = DBSCAN(eps=.4, min_samples=5)
+scan.fit(reducer3.embedding_)
 end_time = time.time()
 print(f"DBSCAN clustering time: {end_time - start_time:.3f} seconds")
+
+dft= pd.read_csv('../01_start/parts.csv')
+dff= pd.read_csv('../01_start/fulltext.csv')
+embedding = reducer2.embedding_
+plt.scatter(embedding[:,0], embedding[:,1], c=scan.labels_, cmap='Spectral', s=5)
+plt.gca().set_aspect('equal','datalim')
+plt.colorbar() #boundaries=np.arange(11)-.5).set_ticks(np.arange(10))
+plt.savefig('umap_dbscan.png', dpi=300)
+
+dft2 = dft.drop(columns=['Unnamed: 0'])
+dft2['cluster'] = scan.labels_
+
+p = umap.plot.interactive(reducer2, labels=scan.labels_, hover_data=dft2, point_size=4, width=1800, height=900)
+umap.plot.show(p)
+
+# Print information about the clusters
+print("Number of clusters found:", len(set(scan.labels_)) - (1 if -1 in scan.labels_ else 0))
+
+
 
 def main():
     print("Hello from 02-gpu!")
