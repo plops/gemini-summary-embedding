492cd57fb3e5275470833d0a02798366411023f7
Martin Kielhorn <wolpumba@gmail.com>
2025-07-27 12:00:54 +0200

    cuml example works
    embed/02_gpu $ uv run main.py
    0    0
    1    1
    2    2
    dtype: int32
    Hello from 02-gpu!

---
+++ 02_gpu/README.md
@@ -0,0 +1,7 @@
+
+
+install cuml with `uv pip install cuml-cu12 cudf-cu12`
+
+perform `uv sync` to build the local environment
+
+run `uv run main.py`
--- 02_gpu/main.py
+++ 02_gpu/main.py
@@ -1,3 +1,20 @@
+import cudf
+from cuml.cluster import DBSCAN
+# export CUDA_PATH=/opt/cuda/
+
+# Create and populate a GPU DataFrame
+gdf_float = cudf.DataFrame()
+gdf_float['0'] = [1.0, 2.0, 5.0]
+gdf_float['1'] = [4.0, 2.0, 1.0]
+gdf_float['2'] = [4.0, 2.0, 1.0]
+
+# Setup and fit clusters
+dbscan_float = DBSCAN(eps=1.0, min_samples=1)
+dbscan_float.fit(gdf_float)
+
+print(dbscan_float.labels_)
+
+
 def main():
     print("Hello from 02-gpu!")
 
